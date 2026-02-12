d88faf328853cbeb0963cde6ca9c75b4806de74f
dafadsf-dev <300090908@fvtc.edu>
2026-02-11 20:09:26 -0600

    a update

--- site.js
+++ site.js
@@ -1,7 +1,13 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const h1 = document.querySelector("h1");
+const h1 = document.querySelector("h1")
 
-  h1.addEventListener("mouseover", function () {
-    h1.textContent = "Welcome to my website!";
-  });
-});
+const hours = new Date().getHours() // get the current hour
+
+const isMorning = hours >= 4 && hours < 12 // is it morning?
+const isAfternoon = hours >= 12 && hours < 17 // is it afternoon?
+const isEvening = hours >= 17 || hours < 4 // is it evening?
+ if (isMorning) 
+  { h1.textContent="Good Morning!" } 
+ else if (isAfternoon) {
+  h1.textContent="Good Afternoon!" }
+ else {
+  h1.textContent="Good Evening!" }
